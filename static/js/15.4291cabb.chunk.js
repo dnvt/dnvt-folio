482d74001424406167d11f655e52162eef79d1b0
franzwa <francois@dnvt.me>
2021-08-23 12:59:43 +0200

    Updates

--- static/js/15.4291cabb.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpdnvt-folio"]=this["webpackJsonpdnvt-folio"]||[]).push([[15],{131:function(t,e,s){"use strict";s.r(e);s(1);var n=s(9),c=s(88),i=s(93),j=s(5),o=s(0);e.default=function(){return Object(o.jsxs)(o.Fragment,{children:[Object(o.jsx)(j.a,{contained:!0,height:40}),Object(o.jsx)(i.a,{positionNotfixed:!0}),Object(o.jsx)(n.a,{}),Object(o.jsx)(c.a,{}),Object(o.jsx)(n.a,{})]})}}}]);
-//# sourceMappingURL=15.4291cabb.chunk.js.map
